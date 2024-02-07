c9a0b67408dc3dd25d8a89c12ea66c5cbe0462d6
DEV-DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-02-07 18:41:45 -0500

    feat (misc): slight changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.cameraserver.CameraServer;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -74,6 +75,8 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
+    
+
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
@@ -85,6 +88,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testInit() {
+    CameraServer.startAutomaticCapture();
+
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
